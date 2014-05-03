70f93889d4565a7f2bb5c7fd65cc260c974668f2
stasonhan <huade20@126.com>
2014-05-03 08:53:02 -0700

    sql_server.c

--- sql_server.c
+++ sql_server.c
@@ -5,11 +5,17 @@
 #include "command.h"
 int thread_num = 0;
 task_queue_t task;
+void signal_quit()
+{
+    destroy_task(&task);
+    exit(2);
+}
 int main()
 {
 
     init_task(&task);
     signal(SIGPIPE,SIG_IGN);
+    signal(SIGINT,signal_quit);
     create_listen_thread();
     create_worker_thread(); 
     
