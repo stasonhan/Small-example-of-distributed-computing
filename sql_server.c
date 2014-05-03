4e4ee36713cb58cb8b708fb8cc986a83efb5a7e8
stasonhan <huade20@126.com>
2014-05-03 08:42:54 -0700

    sql_server.c

--- sql_server.c
+++ sql_server.c
@@ -2,12 +2,13 @@
 #include <signal.h>
 #include "sock.h"
 #include "db_interface.h"
-
+#include "command.h"
 int thread_num = 0;
 task_queue_t task;
 int main()
 {
 
+    init_task(&task);
     signal(SIGPIPE,SIG_IGN);
     create_listen_thread();
     create_worker_thread(); 
